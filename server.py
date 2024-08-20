a670710d59a880c49ddfb82c903f799339a7bd13
namaek2 <science4588@gmail.com>
2024-08-21 06:20:21 +0900

    asdf

--- server.py
+++ server.py
@@ -96,11 +96,33 @@ def download_file(url, client_ip):
     None
   """
   save_path = "./sus.pdf"
+  params = {
+    "table": "block",
+    "id": "4092cd2c-1caa-4050-ae52-47616d469416",
+    "spaceId": "1555847c-ad04-4775-bf98-dc41a42ab25c",
+    "expirationTimestamp": "1724277600000",
+    "signature": "JYGwHJmNbM0t7_gwU7SDq9o3UqED9VpS7d_lKoS4l_w",
+    "downloadName": "0a0c1a9d853ca6221456b690f826cec7.pdf"
+}
+
   headers = {
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+        "Host": "file.notion.so",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "DNT": "1",
+        "Sec-GPC": "1",
+        "Connection": "keep-alive",
+        "Cookie": "file_token=v02%3Afile_token%3AMGuAlpEqFzRdJGTSKYtKf4kLV5FWlrv6fqrXYEx_Vk-HAhpoFBjx9ncmS_fPkMVtbVEBFRF1BSWisuektAMqWidICHuPylz_-ohP4kXLHMFa9RNhK0jdVVALeIQ5t_QbwbNrDeii2iU0LEZV2rTQa8AYCSap; amp_af43d4=c5fc077503dd4474b3fe551100961f2e.ZmEyZjZhNDUyNTlmNDg5NjgxNDdiYzVmODEyYTNjYmY=..1i5oo93j8.1i5opslrs.69s.68.6g4; _cioid=fa2f6a45259f48968147bc5f812a3cbf; _gcl_au=1.1.614511954.1723543468; _ga_9ZJ8CB186L=GS1.1.1723543468.1.1.1723543724.60.0.0; _ga=GA1.1.340125454.1723543468; _rdt_uuid=1723543469049.8427512d-c926-4fea-ae09-0eaec91ba9bc; _hjSessionUser_3664679=eyJpZCI6IjEyNzNhMmIwLWRlZjYtNWNiOC1iZWQ4LWJmY2U5ODYzMGM5NyIsImNyZWF0ZWQiOjE3MjM1NDM0Njk2MDksImV4aXN0aW5nIjpmYWxzZX0=; _cfuvid=z13kUUgN29nUXv6Qja1je_JSQCQcyox9rstpZ2gb8V4-1724114043815-0.0.1.1-604800000; __cf_bm=dcj3zdf5Ew_T183_6JHwQnipwMHgBpXRGbl4rogUrzI-1724187598-1.0.1.1-yJSZSYZcrVhNbQt51v.ve8ahrbfWHOglLj8UxaphsA5xDRjfP2e.cs1umHW1AnebloWJ.2fI.dQsXA_ArYrV8g",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1"
     }
   try:
-    response = requests.get(url, headers=headers, stream=True)
+    response = requests.get(url, headers=headers, params=params, stream=True)
     response.raise_for_status()  # 예외 발생 시 HTTPError 발생s
 
     with open(save_path, 'wb') as f:
