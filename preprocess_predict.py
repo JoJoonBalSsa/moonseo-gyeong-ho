f257c7323cfb4a03b8768bb47f0749e1555856a9
namaek2 <science4588@gmail.com>
2024-08-21 10:01:02 +0900

    test

--- preprocess_predict.py
+++ preprocess_predict.py
@@ -5,10 +5,11 @@ import fitz  # PyMuPDF
 
 # PDF 파일에서 바이트 데이터 추출 (200KB 한정)
 def extract_bytes_from_pdf(pdf_path):
-    with fitz.open(pdf_path) as doc:
+    with open(pdf_path, 'rb') as file:
+    #with fitz.open(pdf_path) as file:
         byte_data = []
-        for page_num in range(len(doc)):
-            page = doc.load_page(page_num)
+        for page_num in range(len(file)):
+            page = file.load_page(page_num)
             text = page.get_text("text").encode('utf-8')
             byte_vector = np.frombuffer(text, dtype=np.uint8)
             if len(byte_vector) > 20000:
@@ -19,7 +20,7 @@ def extract_bytes_from_pdf(pdf_path):
     return byte_data
 
 
-#
+
 def use_model(model_path, scaler_path, pdf_path):
     # iForest 모델 및 스케일러 로드
     iforest = joblib.load(model_path)
@@ -30,13 +31,20 @@ def use_model(model_path, scaler_path, pdf_path):
 
     # 데이터 정규화
     byte_data_normalized = scaler.transform(bytes_from_pdf)
-
     # 모델을 사용한 예측
     predictions = iforest.predict(byte_data_normalized)
-
     index = []
     # 예측 결과 출력: -1은 이상치, 1은 정상
     for i, pred in enumerate(predictions):
         if pred == -1:
           index.append(i)
     return index
+
+def test():
+    model_path = './iforest_byte_model7.pkl'
+    scaler_path = './scaler7.pkl'
+    pdf_path = './0e419f50ed7a7eeee7436c73acbd2d30N.pdf'  # 테스트용 PDF 경로 설정
+    index = use_model(model_path, scaler_path, pdf_path)
+    print(index)
+
+test()
